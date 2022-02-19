8a8287062dae7fcb19de7b7f58bb3625372588fc
romankt21 <romankt21@gmail.com>
2022-02-19 20:56:52 +0200

    lesson4_hw

---
+++ js_les4/js_les4_hw/js_les4_hw.js
@@ -0,0 +1,93 @@
+// +1 створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+//function areaRectangle(a, b){
+
+//    return  a * b ;
+//}
+//console.log("площа прямокутника = " + areaRectangle(2, 3))
+
+//створити функцію яка обчислює та повертає площу кола з радіусом r
+
+//function areaSquare(r){
+//    return 3.14 * r ** 2;
+//}
+//console.log("площа кола = "+ areaSquare(10))
+
+
+// 2.- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+//function areaСylinder(h, r) {
+
+// return 2 * 3.14 * r * + h
+//}
+//console.log("площа циліндру "+areaСylinder( 5, 1))
+//2 π R(R + h)
+
+
+
+// 3.- створити функцію яка приймає масив та виводить кожен його елемент
+// let array = [1, 25, 365, 1000];
+// function narray (array) {
+//     for (let i = 0; i < array; i++) {
+//         console.log(array)
+//     }
+//     return array
+// }
+// console.log(array)
+
+// 4.- створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+//function text (t) {
+//    return text
+// }
+// console.log(`<p>{to}</p>`)
+
+//5.створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function divcreator (conv) {
+//     for (j = 0 ; j < conv; j++) {
+//         document.write(`<li> test </li>`)
+//     }
+// }
+// divcreator(3)
+
+
+ // function cul(arg) {
+//document.write(`<ul>`)
+//     document.write(`<li>${arg}</li>`)
+ //     document.write(`<li>${arg}</li>`)
+ //      document.write(`<li>${arg}</li>`)
+//       document.write(`</ul>`)
+//  }
+ //  cul('Text');
+
+
+// 6.- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+//   function uls(arg, number) {
+//       document.write(`<ul>`)
+//       for (let i = 0; i  <  number; i++) {
+//           document.write(`<li>${arg}</li>`);
+//       }
+//       document.write(`</ul>`)
+//  }
+//   uls('texts', 3);
+
+
+//7 створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+//  let array = [ 1, 2022, 'str', 'table', true, false ] ;
+// function all (array) {
+//      document.write(`<f>`)
+//      for (let i = 0; i  <  array.length; i++) {
+//          document.write(`<li>${array[i]}</li>`);
+//      }
+//      document.write(`</f>`)
+//  }
+//      all(array);
+
+// 8.- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+let array = [{id: 1, name: 'Stepan', age: 35}, {id: 2, name: 'Vasyl', age: 40}, {id: 3, name: 'Stepan', age: 35}];
+function nearray (array) {
+   for (let arrayElement of array)
+    document.write(`<div>${arrayElement.id}.${arrayElement.name} - ${arrayElement.age} </div>`);
+}
+nearray(array);
+
+
+
